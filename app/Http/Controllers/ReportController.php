1d61df9fe93fc848d8196b3c1b6595a9386e16d7
developer2 <degomaralphreigh@yahoo.com>
2016-04-14 01:31:26 +0800

    Reports adding bar graphs and fixing data

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -71,19 +71,8 @@ class ReportController extends Controller
 
         if(Request::input("sub_category") == "All"){
 
-            $income = db::table('tblviewreportincome')
-                ->where('year','>=',Request::input('start'))
-                ->where('year','<=',Request::input('end'))
-                ->orderby('year','asc')
-                ->orderby('person_id','asc')
-                ->get();
+            return $this->allFarmerReport();
 
-            $expenses = db::table('tblviewreportexpenses')
-                    ->where('year','>=',Request::input('start'))
-                    ->where('year','<=',Request::input('end'))
-                    ->orderby('year','asc')
-                    ->orderby('person_id','asc')
-                    ->get();
         }else{
 
             
@@ -108,61 +97,58 @@ class ReportController extends Controller
         }
 
 
-        $final =  array();
+        $person =  array();
         $av =  array();
         $year =  array();
         $tempYear = "";
-
-
+        $personHolder = "";
         $finalData = array();
+
+      
+        
 
         foreach ($income as $key => $value) {
             
             if($tempYear == ""){
                 $tempYear =  $value->year;
-                $final[][] = $value;
                 $year[] = $value->year;
             }
 
             if($tempYear != $value->year){
 
                 $tempYear =  $value->year;
-                $final[][] = $value;
                 $year[] = $value->year;
 
             }
 
         }
 
-        
         foreach($year as $yearValue){
 
             foreach($income as $dataValue){
 
-                if($yearValue == $dataValue->year){
+                    if($yearValue == $dataValue->year){
 
-                    $finalIncomeData[$yearValue][] = $dataValue;
+                        $finalIncomeData[$yearValue][] = $dataValue;
+                    }
                 }
-            }
         }
 
         
-
-
-
-
+        
+        
+        $year = "";
+        
         foreach ($expenses as $key => $value) {
             
             if($tempYear == ""){
                 $tempYear =  $value->year;
-                $final[][] = $value;
                 $year[] = $value->year;
             }
 
             if($tempYear != $value->year){
 
                 $tempYear =  $value->year;
-                $final[][] = $value;
                 $year[] = $value->year;
 
             }
@@ -188,21 +174,8 @@ class ReportController extends Controller
 
         $sorter = Request::all();
 
-/*        return view('bis.reports.trackYears')
-                    ->with('finalExpensesData',$finalExpensesData)
-                    ->with('sorter',$sorter)
-                    ->with('finalIncomeData',$finalIncomeData);
-*/
-        if(Request::input("sub_category") == "All"){
-
-            $pdf = Pdf::loadView('bis.reports.trackYearsAllPeople',['sorter'=> $sorter,'finalExpensesData' => $finalExpensesData,'finalIncomeData' => $finalIncomeData]);
-            
-            dd($finalExpensesData);
-
-        }else{
-
-            $pdf = Pdf::loadView('bis.reports.trackYears',['sorter'=> $sorter,'finalExpensesData' => $finalExpensesData,'finalIncomeData' => $finalIncomeData]);
-        }
+        $pdf = Pdf::loadView('bis.reports.trackYears',['sorter'=> $sorter,'finalExpensesData' => $finalExpensesData,'finalIncomeData' => $finalIncomeData]);
+        
      
         return $pdf->setPaper('letter')->setOrientation('portrait')
                     ->setOption('margin-bottom', 10)
@@ -212,6 +185,181 @@ class ReportController extends Controller
                     ->setOption('javascript-delay', 1000)
                     ->stream('reports.pdf');
 
+    }
+
+    public function allFarmerReport(){
+
+        $income = db::table('tblviewreportincome')
+                ->where('year','>=',Request::input('start'))
+                ->where('year','<=',Request::input('end'))
+                ->orderby('person_id','asc')
+                ->orderby('year','asc')
+                ->get();
+
+        $expenses = db::table('tblviewreportexpenses')
+                ->where('year','>=',Request::input('start'))
+                ->where('year','<=',Request::input('end'))
+                ->orderby('person_id','asc')
+                ->orderby('year','asc')
+                ->get();
+
+
+        if(count($income) == 0){
+            return "nothing to show";
+        }
+
+
+        $person =  array();
+        $av =  array();
+        $year =  array();
+        $tempYear = "";
+        $personHolder = "";
+        $finalData = array();
+
+
+         /*PERSON LOOPING*/
+
+        foreach ($income as $key => $value) {
+            
+            if($personHolder == ""){
+                $personHolder =  $value->person_id;
+                $person[] = $value->person_id;
+            }
+            if($personHolder != $value->person_id){
+
+                $personHolder =  $value->person_id;
+                $person[] = $value->person_id;
+            }
+        }
+        
+        
+
+         /*INCOME LOOPING*/
+        
+       
+        $yearCount = 0;
+        foreach ($income as $key => $value) {
+            
+            if($tempYear == ""){
+                $tempYear =  $value->year;
+                $year[] = $value->year;
+            }
+
+            if($tempYear != $value->year){
+
+                foreach ($year as $keyYear => $yearVal) {
+
+                    if($yearVal == $value->year){
+                        $yearCount++;
+                    }
+
+                }
+
+                if($yearCount == 0 ){
+
+                        $tempYear =  $value->year;
+                        $year[] = $value->year;
+                }
+            }
+
+        }   
+
+
+        foreach($person as $personValue){
+
+
+            foreach($income as $dataValue){
+                
+                if($personValue == $dataValue->person_id){
+
+                    foreach($year as $yearValue){
+
+                        if($yearValue == $dataValue->year){
+
+                            $finalIncomeData[$personValue][$yearValue][] = $dataValue;
+                        }
+                    }
+                }
+
+            }
+        }
+
+
+
+        /*EXPENSES LOOPING*/
+        $year = [];
+
+        $yearCount = 0;
+        foreach ($expenses as $key => $value) {
+            
+            if($tempYear == ""){
+                $tempYear =  $value->year;
+                $year[] = $value->year;
+            }
+
+            if($tempYear != $value->year){
+
+                foreach ($year as $keyYear => $yearVal) {
+
+                    if($yearVal == $value->year){
+                        $yearCount++;
+                    }
+
+                }
+
+                if($yearCount == 0 ){
+
+                        $tempYear =  $value->year;
+                        $year[] = $value->year;
+                }
+            }
+
+        }
+
+
+        foreach($person as $personValue){
+
+            foreach($expenses as $dataValue){
+
+                if($personValue == $dataValue->person_id){
+                    
+                    foreach($year as $yearValue){
+
+                        if($yearValue == $dataValue->year){
+
+                            $finalExpensesData[$personValue][$yearValue][] = $dataValue;
+                        }
+                    }
+                }
+
+            }
+        }
+
+
+
+
+
+
+
+        $sorter = Request::all();
+
+        return view('bis.reports.trackYearsAllPeople')
+                ->with('sorter', $sorter)
+                ->with('finalExpensesData', $finalExpensesData)
+                ->with('finalIncomeData', $finalIncomeData);
+
+
+
+        $pdf = Pdf::loadView('bis.reports.trackYearsAllPeople',['sorter'=> $sorter,'finalExpensesData' => $finalExpensesData,'finalIncomeData' => $finalIncomeData]);
+            
+
+        return $pdf->setPaper('letter')->setOrientation('portrait')
+                    ->setOption('margin-bottom', 10)
+                    ->setOption('margin-left', 10)
+                    ->setOption('margin-top', 10)
+                    ->setOption('enable-javascript', true)
+                    ->setOption('javascript-delay', 1000)
+                    ->stream('reports.pdf');
     }
 
     public function bargraph(){
--- resources/views/bis/reports/trackYearsAllPeople.blade.php
+++ resources/views/bis/reports/trackYearsAllPeople.blade.php
@@ -11,232 +11,17 @@
 		</style>
 		<title>INCOME AND EXPENSES</title>
 	</head>
-<body>
-<script type="text/javascript">
-	
-</script>
-	<div class="page-num">Page 1</div>
-	<div class="body-legal col-100" >
-		<div class ="header col-md-12">
-			<div class="col-md-3" style="margin-top: 0px">
-				<img src="http://{{$_SERVER['HTTP_HOST']}}/assets/img/logo.png" height="30px" width= "auto">
-			</div>
-			<div class="col-md-7">
-					<h4 class="t-center bold">SACRED HEART BUTUAN</h4>
-					<p class="t-center">J.C Aquino Ave. Butuan City</p>
-			</div>
-			
-		</div>
-
-		
-		<!-- <div class="col-md-12">
-			<div class = "form-group">
-				<label>Sorted Category: <span class="text-primary">{{$sorter["type"]}}</span></label>
-			</div>
-			<div class = "form-group">
-				<label>Sorted: <span class="text-primary">Sort</span></label>
-			</div>
-			<div class = "form-group">
-				<label>Sort type: <span class="text-primary">Sort</span></label>
-			</div>
-
-		</div>
-			 -->
-		
-
-
-		<div class="col-md-12 text-center text-primary">
-			<h3>INCOME AND EXPENSES</h3>
-			<label class="text-muted"><u><i>Year Range: <span >{{ $sorter["start"] }} - {{ $sorter["end"] }}</i></u></span></label>
-
-		</div>
-
-		<div class="col-md-12 ">
-
-			
-
-			@if(isset($sorter["show_income"]))
-			<div class="text-success">
-				<b><h4>LIST OF INCOMES</h4></b>
-			</div>
-
-			@foreach($finalIncomeData as $key => $value)
-			<div class="form-control text-center " style="background-color:rgb(171, 233, 137)">
-				<label class=""><i>{{ $key }}</i></label>
-			</div>
-			<table class="table  table-condensed table-bordered ">
-					<thead>
-						<tr>
-							<th class="text-center">Income Type</th>
-							<th class="text-center">Rates</th>
-							<th class="text-center">Ave. Value</th>
-						</tr>
-					</thead>
-					
-					<tbody>
-						@foreach($value as $data)
-						<tr>
-							<td>{{ $data->income_item }}</td>
-							<td>{{ $data->income_range }}</td>
-
-							<td>
-								Php {{averageIncome($data)}}.00
-							</td>
-
-							
-						</tr>
-						@endforeach
-
-					</tbody>
-
-					
-
-
-			</table>
-
-
-			<div class="col-md-12" style="margin-bottom: 50px">
-				<div class="pull-right">
-						<label class="text-danger">Total: Php {{ number_format(subtotalIncome($value)) }}.00</label>
-				</div>
-			</div>
-
-			@endforeach
-			@endif
-
-		
-			
-	    	@if(isset($sorter["show_expenses"]))
-			<div class="text-danger">
-				<b><h4>LIST OF EXPENSES</h4></b>
-			</div>
-
-			@foreach($finalExpensesData as $key => $valueExpense)
-			<div class="form-control text-center " style="background-color:rgb(171, 233, 137)">
-				<label class=""><i>{{ $key }}</i></label>
-			</div>
-			<table class="table  table-condensed table-bordered ">
-					<thead>
-						<tr>
-							<th class="text-center">Income Type</th>
-							<th class="text-center">Rates</th>
-							<th class="text-center">Ave. Value</th>
-						</tr>
-					</thead>
-					
-					<tbody>
-						@foreach($valueExpense as $dataExpenses)
-						<tr>
-							<td>{{ $dataExpenses->expenses_item }}</td>
-							<td>{{ $dataExpenses->expenses_range }}</td>
-
-							<td>
-								Php {{averageExpenses($dataExpenses)}}.00
-							</td>
-
-							
-						</tr>
-						@endforeach
-
-					</tbody>
-
-					
-			</table>
-
-
-			<div class="col-md-12" style="margin-bottom: 50px">
-				<div class="pull-right">
-						<label class="text-danger money">Total: Php {{ number_format(subtotalExpenses($valueExpense)) }}</label>
-				</div>
-			</div>
-			@endforeach
-			@endif
-		</div>
-
-
-
-	    <div class="ibox float-e-margins">
-            <div class="ibox-title">
-                <h5>Barchart</h5>
-                <div ibox-tools></div>
-            </div>
-            <div class="ibox-content">
-                <div class="canvas-holder" style="width: 818px; height: 409px;">
-                    <canvas id="barChart" width="1636" height="818" style="width: 818px; height: 409px;"></canvas>
-                </div>
-            </div>
-        </div>
-	<?php
-
-			$subtotal = 0;
-		
-			function subtotalIncome($data){
-
-				$total = 0;
-
-				foreach ($data as $value) {
-					$sub = averageIncome($value);
-					$total += $sub;
-				}
-				
-				return $total;
-			}
-
-			function averageIncome($data){
-
-				if($data->income_start == "Above "){
-					$data = 7000;
-				}else{
-					$data = ($data->income_start + $data->income_end) /2;
-				}
-				
-
-				return $data;
-			}
-
-			function subtotalExpenses($data){
-
-				$total = 0;
-
-				foreach ($data as $value) {
-					$sub = averageExpenses($value);
-					$total += $sub;
-				}
-				
-				return $total;
-			}
-
-			function averageExpenses($dataEx){
-
-
-				if($dataEx->expenses_start == "Above "){
-
-					$dataEx = 7000;
-				}
-				else{
-
-					$dataEx = ($dataEx->expenses_start + $dataEx->expenses_end) /2;
-
-				}
-
-				return $dataEx;
-			}
-	?>
 
 	<script src="http://{{$_SERVER['HTTP_HOST']}}/assets/js/plugins/chartJs/Chart.min.js"></script>
 
 	<script type="text/javascript">
 
-		
-		bargraph();
+
+		function bargraph(id,dataset){
 
 
-		function bargraph(){
-
-
-			var dataoBj =  <?php echo json_encode($finalIncomeData) ?>;
-			var dataoBj2 =  <?php echo json_encode($finalExpensesData) ?>;
-			var div = "barChart";
+			var dataoBj =  dataset;
+			var div = id;
 
 			var years = [];
 
@@ -271,25 +56,7 @@
 				income = 0;
 			}
 
-			for(s in dataoBj2){
-
-
-				for(sub2 in dataoBj2[s]){
-
-					var expenses_start = dataoBj2[s][sub2].expenses_start;
-					var expenses_end = dataoBj2[s][sub2].expenses_end;
-
-					if(expenses_start == "Above "){
-						expenses_start = 7000;
-					}
-
-					var tempAverageExpenses = (parseFloat(expenses_start)+parseFloat(expenses_end))/2;
-					expenses += tempAverageExpenses;
-				}	
-
-				expensesFinal.push(expenses);
-				expenses = 0;
-			}
+		
 
 
 			console.log(incomeFinal);
@@ -298,21 +65,14 @@
 		        labels: years,
 		        datasets: [
 		            {
-		                label: "My First dataset",
+		                label: "Income",
 		                fillColor: "rgba(26,179,148,0.5)",
 		                strokeColor: "rgba(220,220,220,0.8)",
 		                highlightFill: "rgba(220,220,220,0.75)",
 		                highlightStroke: "rgba(220,220,220,1)",
 		                data: incomeFinal
-		            },
-		            {
-		                label: "My Second dataset",
-		                fillColor: "#C90887",
-		                strokeColor: "rgba(26,179,148,0.8)",
-		                highlightFill: "rgba(26,179,148,0.75)",
-		                highlightStroke: "rgba(26,179,148,1)",
-		                data:expensesFinal
 		            }
+		           
 		        ]
 		    };
 
@@ -320,11 +80,11 @@
 		        scaleBeginAtZero: true,
 		        scaleShowGridLines: true,
 		        scaleGridLineColor: "rgba(0,0,0,.05)",
-		        scaleGridLineWidth: 1,
-		        barShowStroke: true,
+		        scaleGridLineWidth: 5,
+		        barShowStroke: false,
 		        barStrokeWidth: 2,
-		        barValueSpacing: 5,
-		        barDatasetSpacing: 1,
+		        barValueSpacing: 62,
+		        barDatasetSpacing: 2,
 		        responsive: true,
 		    }
 
@@ -377,6 +137,271 @@
 
 
 	</script>
+<body>
+<script type="text/javascript">
+	
+</script>
+	<div class="page-num">Page 1</div>
+	<div class="body-legal col-100" >
+		<div class ="header col-md-12">
+			<div class="col-md-3" style="margin-top: 0px">
+				<img src="http://{{$_SERVER['HTTP_HOST']}}/assets/img/logo.png" height="30px" width= "auto">
+			</div>
+			<div class="col-md-7">
+					<h3 class="t-center bold">SACRED HEART OF BUTUAN</h3>
+					<p class="t-center">J.C Aquino Ave. Butuan City</p>
+			</div>
+			
+		</div>
+
+		
+		<!-- <div class="col-md-12">
+			<div class = "form-group">
+				<label>Sorted Category: <span class="text-primary">{{$sorter["type"]}}</span></label>
+			</div>
+			<div class = "form-group">
+				<label>Sorted: <span class="text-primary">Sort</span></label>
+			</div>
+			<div class = "form-group">
+				<label>Sort type: <span class="text-primary">Sort</span></label>
+			</div>
+
+		</div>
+			 -->
+		
+
+
+		<div class="col-md-12 text-center text-primary" style="margin-top: 70px">
+			<h4>INCOME AND EXPENSES</h4>
+			<label class="text-muted"><u><i>Year Range: <span >{{ $sorter["start"] }} - {{ $sorter["end"] }}</i></u></span></label>
+
+		</div>
+
+		<div class="col-md-12 ">
+
+			
+
+			@if(isset($sorter["show_income"]))
+				<div class="text-danger">
+					<b><h4>LIST OF INCOMES</h4></b>
+				</div>
+			@foreach($finalIncomeData as $yearData)
+				
+
+				<div class="text-primary">
+						<b><h5>{{incomeInfo($yearData)}}</h5></b>
+				</div>
+
+				@foreach($yearData as $key => $value)
+
+				
+
+				<div class="form-control text-center " style="background-color:rgb(171, 233, 137)">
+					<label style="color: white"><i>{{ $key }}</i></label>
+				</div>
+				<table class="table  table-condensed table-bordered ">
+						<thead>
+							<tr>
+								<th class="text-center">Income Type</th>
+								<th class="text-center">Rates</th>
+								<th class="text-center">Ave. Value</th>
+							</tr>
+						</thead>
+						
+						<tbody>
+							@foreach($value as $data)
+							<tr>
+								<td>{{ $data->income_item }}</td>
+								<td>{{ $data->income_range }}</td>
+
+								<td>
+									Php {{averageIncome($data)}}.00
+								</td>
+
+								
+							</tr>
+							@endforeach
+
+						</tbody>
+
+						
+
+
+				</table>
+
+
+				<div class="col-md-12" style="margin-bottom: 50px">
+					<div class="pull-right">
+							<label class="text-danger">Total: Php {{ number_format(subtotalIncome($value)) }}.00</label>
+					</div>
+				</div>
+
+				@endforeach
+
+
+
+				<div class="ibox float-e-margins">
+		            <div class="ibox-title">
+		                <h5>Barchart</h5>
+		                <div ibox-tools></div>
+		            </div>
+		            <div class="ibox-content">
+		                <div class="canvas-holder" style="width: 818px; height: 409px;">
+		                    <canvas id="barChart{{$key}}" width="1636" height="818" style="width: 818px; height: 409px;"></canvas>
+		                </div>
+		            </div>
+		        </div>
+
+		        <script type="text/javascript"> 
+		        		var id = "barChart{{$key}}";
+		        		var dataSet = <?php echo json_encode($yearData) ?>;
+		        		bargraph(id,dataSet); 
+
+		        </script>
+			@endforeach
+			@endif
+
+		
+			
+	    	@if(isset($sorter["show_expenses"]))
+			
+		
+	    		<div class="text-danger">
+					<b><h4>LIST OF EXPENSES</h4></b>
+				</div>
+
+			@foreach($finalExpensesData as $person_id => $yearExpensesData)
+
+				<div class="text-primary">
+						<b><h5>{{incomeInfo($yearExpensesData)}}</h5></b>
+				</div>
+
+				
+					@foreach($yearExpensesData as $key => $valueExpense)
+
+				
+
+					<div class="form-control text-center " style="background-color:#DA425A">
+						<label style="color: white"><i>{{ $key }}</i></label>
+					</div>
+					<table class="table  table-condensed table-bordered ">
+							<thead>
+								<tr>
+									<th class="text-center">Expenses Type</th>
+									<th class="text-center">Rates</th>
+									<th class="text-center">Ave. Value</th>
+								</tr>
+							</thead>
+							
+							<tbody>
+								@foreach($valueExpense as $dataExpenses)
+								<tr>
+									<td>{{ $dataExpenses->expenses_item }}</td>
+									<td>{{ $dataExpenses->expenses_range }}</td>
+
+									<td>
+										Php {{averageExpenses($dataExpenses)}}.00
+									</td>
+
+									
+								</tr>
+								@endforeach
+
+							</tbody>
+
+							
+					</table>
+
+
+					<div class="col-md-12" style="margin-bottom: 50px">
+						<div class="pull-right">
+								<label class="text-danger money">Total: Php {{ number_format(subtotalExpenses($valueExpense)) }}</label>
+						</div>
+					</div>
+					@endforeach
+				@endforeach
+			@endif
+		</div>
+
+
+
+	    <div class="ibox float-e-margins">
+            <div class="ibox-title">
+                <h5>Barchart</h5>
+                <div ibox-tools></div>
+            </div>
+            <div class="ibox-content">
+                <div class="canvas-holder" style="width: 818px; height: 409px;">
+                    <canvas id="barChart" width="1636" height="818" style="width: 818px; height: 409px;"></canvas>
+                </div>
+            </div>
+        </div>
+	<?php
+
+			$subtotal = 0;
+		
+			function subtotalIncome($data){
+
+				$total = 0;
+
+				foreach ($data as $value) {
+					$sub = averageIncome($value);
+					$total += $sub;
+				}
+				
+				return $total;
+			}
+
+			function incomeInfo($data){
+
+				foreach ($data as $year => $value) {
+					return $value[0]->last_name.", ".$value[0]->first_name." ".$value[0]->middle_name;
+				}
+				
+			}
+
+			function averageIncome($data){
+
+				if($data->income_start == "Above "){
+					$data = 7000;
+				}else{
+					$data = ($data->income_start + $data->income_end) /2;
+				}
+				
+
+				return $data;
+			}
+
+			function subtotalExpenses($data){
+
+				$total = 0;
+
+				foreach ($data as $value) {
+					$sub = averageExpenses($value);
+					$total += $sub;
+				}
+				
+				return $total;
+			}
+
+			function averageExpenses($dataEx){
+
+
+				if($dataEx->expenses_start == "Above "){
+
+					$dataEx = 7000;
+				}
+				else{
+
+					$dataEx = ($dataEx->expenses_start + $dataEx->expenses_end) /2;
+
+				}
+
+				return $dataEx;
+			}
+	?>
+
+	
 
 </body>
 
